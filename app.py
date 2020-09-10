7c302d59eb1e047e2061451b76d604cbb8cd6a1f
Rohit Verma <63773928+rohit0906@users.noreply.github.com>
2020-09-10 23:32:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model_placement = pickle.load(open('placement.pkl', 'rb'))
+model_salary=pickle.load(open('salary.pkl', 'rb'))
+
+#['ssc_p',
+# 'hsc_p',
+# 'degree_p',
+# 'etest_p',
+# 'mba_p',
+# 'gender_M',
+# 'degree_t_Sci&Tech',
+# 'workex_Yes']
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model_placement.predict(final_features)
+
+    out={'Not Placed':'No', 'Placed':'Yes'}
+
+    output=out[prediction[0]]
+    if output=='Yes':
+        salary = model_salary.predict(final_features)
+        salary=round(salary[0],-5)*12/100000
+        return render_template('index.html', prediction_text=' {}'.format(output)+'     Approx. Salary: {}LPA'.format(salary))
+
+    if output == 'No':
+        return render_template('index.html', prediction_text=' {}'.format(output))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
